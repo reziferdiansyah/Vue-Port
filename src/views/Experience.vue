aeabceaa5433cdc7759a3db5726973d1acb5c9b3
reziferdiansyah <reziferdiansyah23>
2024-10-25 08:47:57 +0700

    update-experience

--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -1,3 +1,33 @@
 <template>
-    <div>TEST EXPERience</div>
-</template>
+    <div>
+      <!-- About me -->
+      <div class="container mx-auto">
+        <Experience />
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import Experience from "@/components/experience/Experience";
+  import feather from "feather-icons";
+  
+  export default {
+    name: "About",
+    components: {
+      Experience,
+    },
+    data: () => {
+      return {};
+    },
+    mounted() {
+      feather.replace();
+    },
+    updated() {
+      feather.replace();
+    },
+    methods: {},
+  };
+  </script>
+  
+  <style scoped></style>
+  
